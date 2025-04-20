29695712e402a927c636257f92d48395ebdb51bb
Sagnik Saha <sahasagnik279@gmail.com>
2025-04-20 20:54:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const OpenAI = require("openai");
-const path = require("path");  // Add this line
+const path = require("path"); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
