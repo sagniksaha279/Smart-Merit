44d035b100640faf7b9da8cb4eb6e4b7a836851b
Sagnik Saha <sahasagnik279@gmail.com>
2025-04-20 23:12:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   connectTimeout: 10000,    
-  acquireTimeout: 10000,
+ // acquireTimeout: 10000,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
